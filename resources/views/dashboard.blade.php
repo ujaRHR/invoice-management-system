4365282824c63b37fce65c4c97e58fc57ca9c8d9
ujaRHR <rhraju.dev@gmail.com>
2025-01-02 11:35:45 +0600

    Installed: Tailwind with Flowbite

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,15 @@
-<div>
-    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+
+<body>
+    <p class="text-red-500 font-bold ">Initial Demo Page...</p>
+</body>
+
+</html>
