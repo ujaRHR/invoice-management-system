c08cbb1a2dd5b1c638e3616172baeb985d35719d
ujaRHR <rhraju.dev@gmail.com>
2025-01-16 17:40:22 +0600

    Added: Client Deletion

--- resources/views/components/clients/clients-list.blade.php
+++ resources/views/components/clients/clients-list.blade.php
@@ -91,14 +91,14 @@
                             <td class="p-4 text-base font-medium text-gray-800 border border-gray-300 whitespace-nowrap dark:text-white" id="clientCountry">${item['country']}</td>
 
                             <td class="p-4 space-x-2 whitespace-nowrap border border-gray-300">
-                                <button type="button" data-modal-target="edit-client-modal" data-id-${item['id']} data-modal-toggle="edit-client-modal" class="inline-flex items-center px-2 py-1 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                                <button type="button" data-modal-target="edit-client-modal" data-id="${item['id']}" data-modal-toggle="edit-client-modal" class="editBtn inline-flex items-center px-2 py-1 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                                     <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
                                         <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path>
                                     </svg>
                                     Edit
                                 </button>
-                                <button type="button" data-modal-target="delete-client-modal" data-id-${item['id']} data-modal-toggle="delete-client-modal" class="inline-flex items-center px-2 py-1 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
+                                <button type="button" data-modal-target="delete-client-modal" data-id="${item['id']}" data-modal-toggle="delete-client-modal" class="deleteBtn inline-flex items-center px-2 py-1 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
                                     <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
                                     </svg>
@@ -112,5 +112,10 @@
 
         mainTable.DataTable();
     }
+
+    $('table tbody').on('click', '.deleteBtn', function() {
+        let clientId = $(this).data("id")
+        $('#clientId').val(clientId)
+    })
 </script>
 @endpush
