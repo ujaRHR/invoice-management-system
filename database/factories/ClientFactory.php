e910cfde45e929266482df6f89d54cb89afbbc81
ujaRHR <rhraju.dev@gmail.com>
2025-01-30 21:13:41 +0600

    Fixed: modalsInit() issues

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -10,10 +10,26 @@ class ClientFactory extends Factory
     {
         return [
             'cust_id' => 1,
-            'fullname' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'fullname' => $this->faker->unique()->name(),
+            'email' => $this->faker->unique()->freeEmail(),
             'company' => $this->faker->unique()->company(),
-            'country' => $this->faker->unique()->country(),
+            'country' => $this->faker->randomElement([
+                'United States',
+                'United Kingdom',
+                'Canada',
+                'Germany',
+                'Australia',
+                'India',
+                'France',
+                'United Kingdom',
+                'Italy',
+                'Brazil',
+                'Japan',
+                'South Korea',
+                'Mexico',
+                'Germany',
+                'Ireland'
+            ]),
             'created_at' => now(),
             'updated_at' => now(),
         ];
