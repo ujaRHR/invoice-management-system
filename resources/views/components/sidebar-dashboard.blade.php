55292546b9b16b88f4f308c083005efbd0cadca4
ujaRHR <rhraju.dev@gmail.com>
2025-03-02 02:06:35 +0600

    updated: sidebar panel

--- resources/views/components/sidebar-dashboard.blade.php
+++ resources/views/components/sidebar-dashboard.blade.php
@@ -46,22 +46,17 @@
             <a href="/payment-methods"
               class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group 
               dark:text-gray-200 dark:hover:bg-gray-700">
-              <svg class="w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 
-                    dark:text-gray-400 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                  d="M2 5.5A2.5 2.5 0 0 1 4.5 3h15A2.5 2.5 0 0 1 22 5.5v13a2.5 2.5 0 0 1-2.5 2.5h-15A2.5 2.5 0 0 1 2 18.5v-13ZM4.5 4a1.5 1.5 0 0 0-1.5 1.5v1h18v-1A1.5 1.5 0 0 0 19.5 4h-15ZM21 8H3v10.5A1.5 1.5 0 0 0 4.5 20h15A1.5 1.5 0 0 0 21 18.5V8Zm-6 9.5a.5.5 0 0 1 .5-.5H17a.5.5 0 1 1 0 1h-1.5a.5.5 0 0 1-.5-.5ZM7 17a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm-1-2a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2H7a1 1 0 0 1-1-1Z"
-                  clip-rule="evenodd" />
+              <svg class="w-6 h-6 text-gray-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill="none" d="M17.4 12.6h1l-.3-1.4v-.4l-.2.4-.5 1.4Z" />
+                <path fill="currentColor" fill-rule="evenodd" d="M2 6.3c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-12Zm12.5 3.2c.4 0 .8 0 1.1.2l-.1 1h-.1a2 2 0 0 0-1-.3c-.5 0-.7.3-.7.5s.2.3.7.5c.7.4 1 .8 1 1.3 0 1-.8 1.7-2.2 1.7-.6 0-1.1-.2-1.4-.3l.2-1h.1c.4.2.7.3 1.2.3.4 0 .8-.2.8-.5 0-.2-.2-.3-.7-.6-.5-.2-1.1-.6-1.1-1.3 0-.9 1-1.5 2.2-1.5Zm3.5 0h1l1 4.8h-1.2l-.2-.7H17l-.3.7h-1.3l1.9-4.4c.1-.3.3-.3.7-.3Zm-6.2 0h-1.3l-.8 4.8H11l.8-4.8Zm-4.5 3.3-.1-.7-.5-2.2c0-.3-.3-.3-.6-.4h-2v.1l1.2.5.1.2 1.1 4H8l2-4.7H8.7l-1.3 3.2Z" clip-rule="evenodd" />
               </svg>
               <span class="ml-3" sidebar-toggle-item="">Payment Methods</span>
             </a>
           </li>
           <li>
             <a href="/invoices" class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700">
-              <svg class="w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M6 2a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6.828a2 2 0 0 0-.586-1.414L15.586 2.586A2 2 0 0 0 14.172 2H6Zm6 5a1 1 0 0 1 1-1h3.586L18 7.414V18a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5v4Zm-4 6a1 1 0 1 0 0 2h8a1 1 0 1 0 0-2H8Zm0-3a1 1 0 1 0 0 2h8a1 1 0 1 0 0-2H8Z" clip-rule="evenodd" />
+              <svg class="w-6 h-6 text-gray-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 8h6m-6 4h6m-6 4h6M6 3v18l2-2 2 2 2-2 2 2 2-2 2 2V3l-2 2-2-2-2 2-2-2-2 2-2-2Z" />
               </svg>
               <span class="ml-3" sidebar-toggle-item="">Invoices</span>
             </a>
